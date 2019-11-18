ecca7d55f216ed28658e7386a5b1ecf90f146186
cliffdias <diasclifford@gmail.com>
2019-11-18 14:54:23 +0000

    demo app

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+  cognito: {
+    userPoolClientId: '6tuq5lrbr3pcufudk75fulklfs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-west-2', // e.g. us-east-2
+    authDomainPrefix: 'testlseg', // e.g. leosaml
+    redirect_uri: 'https://d3cos4wdd8htls.cloudfront.net' //e.g. https://d*******.cloudfront.net
+  },
+  api: {
+    invokeUrl: 'https://t17lregqad.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
+  }
+};
